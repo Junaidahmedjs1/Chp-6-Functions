3904aa2f652f211c51875950839680f246d27295
Junaidahmedjs1 <junaidahmed31254@gmail.com>
2024-09-15 18:14:35 +0500

    Chp 6 Half complete

---
+++ app.js
@@ -0,0 +1,111 @@
+// FUNCTIONS CHAPTER STARTED
+
+// Answer No 1
+
+// function repeat(a, b)
+// {console.log(a+b);}
+
+// repeat(5 ,10);
+// repeat(20,40);
+// repeat(102 ,90);
+// repeat(564,602);
+
+// Answer No 2
+
+// function date_time(){
+// var date_time= new Date();
+// console.log(date_time.toLocaleString())
+// }
+// date_time()
+
+
+// Answer No 3
+
+
+// function greetuser(userName,LastName){
+//     console.log(userName + LastName);
+    
+// }
+
+// greetuser("Junaid","Ahmed")
+
+//Answer No 4
+
+
+// var num1 = +prompt("Enter Number")
+// var num2 = +prompt("Enter Number")
+
+// function Sum (){
+//     console.log(`The Sum Of number is ${num1+num2}`);
+    
+// }
+
+// Sum()
+
+// Answer No 5
+
+
+// function calculator(num1,num2){
+
+//      return num1 + num2
+//  }
+// var sum=calculator(10,50);
+// console.log(sum)
+
+// function calculator(num1 ,num2,operator){
+// var result;
+
+// if(operator==='+'){
+//    result = num1 + num2
+//  }
+// else  if(operator==='-'){
+//   result = num1 - num2
+// }
+// else  if(operator==='*'){
+//   result = num1 * num2
+//  }
+// else  if(operator==='/'){
+//      result = num1 / num2
+// }   return result;
+// }
+// var sum= calculator(20,50 ,'*');
+// var sum1 = calculator(20,50 ,'+');
+// var sum2 = calculator(20,50 ,'/');
+// var sum3 = calculator(20,50 ,'-');
+// console.log(` The Mulity${sum} , sum${sum1}, Divison${sum3}, sub ${sum}` );
+
+
+//Answer No 6
+
+
+// function Square(num1){
+//     return num1 ** 2
+// }
+// var result=Square(7);
+// console.log(result);
+
+
+// Answer No 8
+
+
+// function Counter(start,end){
+// var counter=document.querySelector("#counting");
+// counter.innerHTML="";
+// for(start; start<=end; start++){
+//  counter.innerHTML +=start + "<br/>";
+        
+// }
+// }
+
+
+// Answer No 9
+
+// function CalculateShape(){
+// function sub(Base,perpendicular){
+// var Hypotenuse = Base ** 2 + perpendicular  ** 2;
+// console.log(`the value of triangle is : ${Hypotenuse} `)
+// }
+// sub(4,10)
+// }
+
+
